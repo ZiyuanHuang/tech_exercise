a96c786a41d2dd1b45145174b5b6450cd2a600e8
Ziyuan Huang <z73huang@uwaterloo.ca>
2015-04-08 17:11:59 -0400

    gina2

--- ar/views.py
+++ ar/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
 	posts = Post.objects.all()
 	return render(request, 'ar/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'ar/post_detail.html', {'post': post})
