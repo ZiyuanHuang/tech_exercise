626d6e51489bf8586302afb8e0b666ccba2d98f5
Ziyuan Huang <z73huang@uwaterloo.ca>
2015-04-09 21:56:04 -0400

    gina5

--- ar/views.py
+++ ar/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from random import randint
+from django.db.models import Count
 
 def post_list(request):
 	posts = Post.objects.all()
-	#selectedPost = ...
-	return render(request, 'ar/post_list.html', {'posts': posts,})
+	posts = posts.order_by('published_date')
+	n = randint(0,9)
+	selectedPosts = posts[n]
+	return render(request, 'ar/post_list.html', {'posts': posts, 'selectedPosts': selectedPosts,})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'ar/post_detail.html', {'post': post})
+    # n = randint(0,9)
+    # selectedPosts = posts[n]
+    return render(request, 'ar/post_detail.html', {'post': post,})
